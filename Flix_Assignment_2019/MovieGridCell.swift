95e3e0b081d2b709a2fc1d9054db3964ef41b9ef
Daniella Montinola <dmonti15@gmail.com>
2019-10-25 16:44:56 -0500

    Collection view setup

---
+++ Flix_Assignment_2019/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix_Assignment_2019
+//
+//  Created by Daniella Montinola on 10/25/19.
+//  Copyright © 2019 Daniella Montinola. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var moviePosterView: UIImageView!
+}
